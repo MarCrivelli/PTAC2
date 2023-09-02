809e0b5ce8be1aa335ce323c50daf8f614793f8c
MarCrivelli <marcoantonio7crivelli@gmail.com>
2023-09-02 17:51:39 -0300

    a
    a

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import './app.css'
+
+export default function App(){
+    return(
+        <div>
+            <div class="container">
+                <div id="produto">
+                    <img src="paisagemPixel.png"></img>
+                    <img src="florestaPixel.jpg"></img>
+                    <img src="selvaPixel.gif"></img>
+                    <img src="gayPixel.jpg"></img>
+                    <img src="horrorPixel.gif"></img>
+                    <img src="gatinhoPixel.gif"></img>
+                </div>
+            </div>
+        </div>
+    );
+}
